c1d779edb91050b642100b7573e8c93ff7b2ce96
Gitkat91 <52242683+Gitkat91@users.noreply.github.com>
2021-09-30 14:07:32 +0200

    Add files via upload

---
+++ Task 9.js
@@ -0,0 +1,21 @@
+function getVowel(str) 
+
+{
+    var vowel = 'AEIOUaeiou'
+    var vowelsString = "";
+    
+    
+     for(var i = 0; i < str.length ; i++)
+      {
+        if (vowel.indexOf(str[i]) !== -1)
+        {
+     vowelsString =  vowelsString  + str[i] +","
+        }
+        
+       
+      }
+   return vowelsString
+    } 
+    
+    
+    console.log("Vowels:  " + getVowel("katlego"))
