6bb75639ae1af1ea7846658a47b3a4472577a075
Gitkat91 <52242683+Gitkat91@users.noreply.github.com>
2021-10-01 07:38:57 +0200

    Add files via upload

---
+++ Task 10.js
@@ -0,0 +1,15 @@
+function returnCommon(string1 , string2)
+{
+    let results = ' ';
+    for (let i of string1){
+        for(let n of string2){
+            if( n === i){
+                results = results + (n) +","; 
+            }
+        }
+    }
+    return results;
+    
+}
+
+ console.log("Common letters:" + returnCommon("house" , "Computer"))
