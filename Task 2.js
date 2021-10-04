16df0647bcb3d645540af4789e367f54bc0a1f1f
Gitkat91 <52242683+Gitkat91@users.noreply.github.com>
2021-10-04 19:39:32 +0200

    Add files via upload

---
+++ Task 2.js
@@ -0,0 +1,17 @@
+
+    
+     let x,y,z,a,b = 0
+    
+    
+    x = 1 + 1 * 2
+    y = (1 + 1) * 2
+    z = 1 + ( 1 * 2 )
+    a = 1 + 1 * 2 / 2
+    b = (1 + 1 * 2 ) /  2
+    
+    console.log(x)
+    console.log(y)
+    console.log(z)
+    console.log(a)
+    console.log(b)
+    
